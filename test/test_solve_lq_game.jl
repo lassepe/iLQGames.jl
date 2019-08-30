720ac38dc38f9170b6965c15be1c50b094d569cd
lassepe <lasse.peters@tuhh.de>
2019-08-30 15:52:54 -0700

    use GR for faster (and less crashing) plotting

--- test/test_solve_lq_game.jl
+++ test/test_solve_lq_game.jl
@@ -18,6 +18,9 @@ using Profile
 using ProfileView
 using BenchmarkTools
 
+using Plots
+gr()
+
 function generate_1D_pointmass_game()
     # Testing the solver at a simple example: A two-player point mass 1D system.
     # The state composes of position and oritation. Therefore, the system dynamics
@@ -178,12 +181,70 @@ function test_lyapunov(g::FiniteHorizonLQGame)
     return P_lyap, P_lqg
 end
 
-@testset "solve_lq_game" begin
-    g_1D = generate_1D_pointmass_game()
-    test_lyapunov(g_1D)
-    benchmark_solve_lq_game(g_1D)
+#@testset "solve_lq_game" begin
+#    g_1D = generate_1D_pointmass_game()
+#    test_lyapunov(g_1D)
+#    benchmark_solve_lq_game(g_1D)
+#
+#    g_2D = generate_2D_pointmass_game()
+#    test_lyapunov(g_2D)
+#    benchmark_solve_lq_game(g_2D)
+#end;
 
-    g_2D = generate_2D_pointmass_game()
-    test_lyapunov(g_2D)
-    benchmark_solve_lq_game(g_2D)
-end;
+function benchmark_solve_lq_game(g::FiniteHorizonLQGame)
+    b = @benchmark solve_lq_game($g::FiniteHorizonLQGame)
+    show(stdout, "text/plain", b)
+    println()
+end
+
+function profile_solve_lq_game(g::FiniteHorizonLQGame)
+    Profile.clear()
+    @profile begin for i in 1:1000
+            solve_lq_game(g)
+    end end
+    ProfileView.view()
+end
+
+function gen_fake_data()
+    # fake data:
+    nx = 5;
+    nu = 2;
+    N_STEPS = 100;
+
+    A = rand(nx, nx)
+    B = rand(nx, nu)
+
+    P = rand(nu, nx)
+    α = zeros(nu)
+    x0 = rand(nx)
+
+    while any(abs(e) >=1 for e in first(eigen(A+B*P)))
+        P = -rand(nu, nx)
+        println(maximum(abs.(first(eigen(A+B*P)))))
+    end
+
+    xs = [x0]
+    us = []
+    resize!(xs, N_STEPS)
+    resize!(us, N_STEPS-1)
+    for k in 1:(N_STEPS-1)
+        us[k] = P*xs[k] + α
+        xs[k+1] = A*xs[k] + B*us[k]
+    end
+
+    return xs, us
+end
+
+
+function plot_systraj(xs, us)
+    nx = length(first(xs))
+    nu = length(first(us))
+
+    # states
+    px = plot(hcat(xs...)'; layout=(nx, 1))
+
+    # inputs
+    pu = plot(hcat(us...)'; layout=(nu, 1))
+
+    plot(px, pu, layout=2)
+end
