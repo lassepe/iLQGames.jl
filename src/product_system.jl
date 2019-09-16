5d8b2d9c414df5910ff0769ebaf5a55b6eb614f0
lassepe <lasse.peters@tuhh.de>
2019-09-15 19:18:56 -0700

    Hardcoded linearization for car5d

--- src/product_system.jl
+++ src/product_system.jl
@@ -16,6 +16,10 @@ struct ProductSystem{ΔT, nx, nu, xids, uids, np, TS<:NTuple{np, <:ControlSystem
         new{ΔT, nx, nu, Tuple(xids), Tuple(uids), np, TS}(sub_systems)
     end
 end
+xindex(cs::ProductSystem{ΔT, nx, nu, xids}) where {ΔT, nx, nu, xids} = xids
+uindex(cs::ProductSystem{ΔT, nx, nu, xids, uids}) where {ΔT, nx, nu, xids, uids} = uids
+
+sub_systems(cs::ProductSystem) = cs.sub_systems
 
 function dx(cs::ProductSystem{ΔT, nx, nu, xids, uids}, x::SVector{nx},
             u::SVector{nu}, t::AbstractFloat) where {ΔT, nx, nu, xids, uids}
@@ -28,4 +32,19 @@ function dx(cs::ProductSystem{ΔT, nx, nu, xids, uids}, x::SVector{nx},
 
     return SVector{nx}(dx_val)
 end
-# TODO: maybe also overload `linearize` and `integrate` to exploit sparsity
+
+# computing large matrix exponentials is expensive. Therefore, we exploit the
+# sparsity of the dynamics and compute the linearization for each sub_systems
+function linearize_discrete(cs::ProductSystem, x::SVector, u::SVector, t::AbstractFloat)
+    nx = n_states(cs)
+    nu = n_controls(cs)
+    # the full matrices
+    A = @MMatrix zeros(nx, nx)
+    B = @MMatrix zeros(nx, nu)
+    for (xid, uid, sub) in zip(xindex(cs), uindex(cs), sub_systems(cs))
+        lin_sub = linearize_discrete(sub, x[xid], u[uid], t)
+        A[xid, xid] = lin_sub.A
+        B[xid, uid] = lin_sub.B
+    end
+    return LinearSystem{sampling_time(cs)}(SMatrix(A), SMatrix(B))
+end
--- src/toy_systems.jl
+++ src/toy_systems.jl
@@ -45,3 +45,24 @@ function dx(cs::Car5D, x::SVector{5}, u::SVector{2}, t::AbstractFloat)
 
     return @SVector[dx1, dx2, dx3, dx4, dx5]
 end
+
+function linearize_discrete(cs::Car5D, x::SVector{5}, u::SVector{2}, t::AbstractFloat)
+    ΔT = sampling_time(cs)
+    cθ = cos(x[3]) * ΔT
+    sθ = sin(x[3]) * ΔT
+    cϕ = cos(x[4])
+    tϕ = tan(x[4])
+
+    A = @SMatrix [1 0 -x[5]*sθ 0                    cθ;
+                  0 1 x[5]*cθ  0                    sθ;
+                  0 0 1        x[5]*ΔT/(cs.l*cϕ*cϕ) tϕ*ΔT/cs.l;
+                  0 0 0        1                    0;
+                  0 0 0        0                    1]
+
+    B = @SMatrix [0  0;
+                  0  0;
+                  0  0;
+                  ΔT 0;
+                  0  ΔT];
+    return LinearSystem{ΔT}(A, B)
+end
