9c11ee46b3539e4a367316674ab6192610b70b62
lassepe <lasse.peters@tuhh.de>
2019-09-25 17:56:14 -0700

    contiuous_time and timepoints for SystemTrajectory

--- src/system_trajectory.jl
+++ src/system_trajectory.jl
@@ -12,6 +12,9 @@ SystemTrajectory{ΔT}(x::TX, u::TU) where {h, ΔT, nx, nu,
                                           TU<:SizedVector{h,<:SVector{nu}}} = SystemTrajectory{h, ΔT, nx, nu, TX, TU}(x, u)
 sampling_time(t::SystemTrajectory{h, ΔT}) where {h, ΔT} = ΔT
 horizon(t::SystemTrajectory{h}) where {h} = h
+contiuous_time(t::SystemTrajectory, k::Int) = (k-1)*sampling_time(t)
+timepoints(t::SystemTrajectory) = (contiuous_time(t, k) for k in 1:horizon(t))
+
 Base.zero(::Type{<:SystemTrajectory{h, ΔT, nx, nu}}) where{h, ΔT, nx, nu} = SystemTrajectory{ΔT}(zero(SizedVector{h, SVector{nx, Float64}}),
                          zero(SizedVector{h, SVector{nu, Float64}}))
 Base.zero(t::SystemTrajectory) = zero(typeof(t))
