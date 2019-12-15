38b7d55cab564d2d7a39b26e4ba2b5dac5cc987c
lassepe <lasse.peters@mailbox.org>
2019-12-15 13:02:42 -0800

    Better default regularization

--- src/ilq_solver.jl
+++ src/ilq_solver.jl
@@ -1,10 +1,10 @@
 @with_kw struct iLQSolver{TLM, TOM, TQM}
     "The regularization term for the state cost quadraticization."
-    state_regularization::Float64 = 1.0
+    state_regularization::Float64 = 5.0
     "The regularization term for the control cost quadraticization."
-    control_regularization::Float64 = 1.0
+    control_regularization::Float64 = 5.0
     "The initial scaling of the feed-forward term."
-    α_scale_init::Float64 = 0.15
+    α_scale_init::Float64 = 0.5
     "The geometric scaling of the feed-forward term per scaling step in
     backtrack scaling."
     α_scale_step::Float64 = 0.5
