483c6d3dfa5e3719a99762a578cf5329c2cd73ff
lassepe <lasse.peters@mailbox.org>
2020-02-21 18:19:40 +0100

    Update minimal example: longer horizon, fix dynamics

--- examples/minimal_example.jl
+++ examples/minimal_example.jl
@@ -6,31 +6,28 @@ using iLQGames:
 using StaticArrays
 
 # parametes: number of states, number of inputs, sampling time, horizon
-nx, nu, ΔT, game_horizon = 4, 2, 0.1, 100
-# indices of inputs that each player controls
-player_inputs = (SVector(1), SVector(2))
-# choose initial conditions intial conditions...
-x0 = SVector(1, 1, 0, 0.5)
+nx, nu, ΔT, game_horizon = 4, 2, 0.1, 200
 
 # setup the dynamics
 struct Unicycle <: ControlSystem{ΔT,nx,nu} end
-# x = (px, py, phi, v)
-dx(cs::Unicycle, x, u, t) = SVector(cos(x[3]), x[4]sin(x[3]), u[1], u[2])
+# state: (px, py, phi, v)
+dx(cs::Unicycle, x, u, t) = SVector(x[4]cos(x[3]), x[4]sin(x[3]), u[1], u[2])
+# TODO weg
 xyindex(cs::Unicycle) = tuple(SVector(1,2))
 dynamics = Unicycle()
 
-# To setup costs we can derive a custom subtype of `PlayerCost` or simply hand the
-# cost function to the FunctionPlayerCost.
-costs = (# player-1 wants the unicycle to stay close to the origin
-         FunctionPlayerCost{nx,nu}((g, x, u, t) -> (x[1]^2 + x[2]^2 + u[1]^2)),
-         # player-2 wants to keep close to 1 m/s
+# player-1 wants the unicycle to stay close to the origin,
+# player-2 wants to keep close to 1 m/s
+costs = (FunctionPlayerCost{nx,nu}((g, x, u, t) -> (x[1]^2 + x[2]^2 + u[1]^2)),
          FunctionPlayerCost{nx,nu}((g, x, u, t) -> ((x[4] - 1)^2 + u[2]^2)))
 
+# indices of inputs that each player controls
+player_inputs = (SVector(1), SVector(2))
 # the horizon of the game
 g = GeneralGame{player_inputs, game_horizon}(dynamics, costs)
-# get a solver for the game
+
+# get a solver, choose initial conditions and solve (in about 9 ms with AD)
 solver = iLQSolver(g)
-# compute solution (takes about 4ms even though we are using AD)
+x0 = SVector(1, 1, 0, 0.5)
 converged, trajectory, strategies = solve(g, solver, x0)
-# ... and plot it
-plot_traj(trajectory)
+plot_traj(trajectory, g, [:red, :green], player_inputs)
